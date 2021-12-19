0be56ef348ec436339d1596721c64dc5988bd98c
htk <vhxpffltm@naver.coom>
2021-12-19 17:43:55 +0900

    fix variable name and draw bar

--- Algo.py
+++ Algo.py
@@ -13,10 +13,17 @@ class Information():
     SIDE_PAD = 100
     TOP_PAD = 150
 
+    GRADIENTS = [
+        GRAY,
+        (128,128,128),
+        (160,160,160),
+        (192,192,192),
+    ]
+
     def __init__(self,width,height,list):
         self.width = width
         self.height = height
-        self.window = pygame.display.set_mode((width,height),)
+        self.window = pygame.display.set_mode((width,height))
         pygame.display.set_caption("Test.....")
         self.set_list(list)
 
@@ -24,7 +31,7 @@ class Information():
         self.list = list
         self.max_val = max(list)
         self.min_val = min(list)
-        self.block_witdh = (self.width - self.SIDE_PAD) / len(list)
+        self.block_width = (self.width - self.SIDE_PAD) / len(list)
         self.block_height = (self.height - self.TOP_PAD) / (self.max_val - self.min_val)
         self.start_x = self.SIDE_PAD // 2
 
@@ -33,19 +40,28 @@ def GenerateList(n,min_val,max_val):
     return lst
 
 def draw(draw_info):
-    pass
+    draw_info.window.fill(draw_info.WHITE)
+    draw_bar(draw_info)
+    pygame.display.update()
 
+def draw_bar(draw_info):
+    for i,j in enumerate(draw_info.list):
+        x = draw_info.start_x + i * draw_info.block_width
+        y = draw_info.height - (j - draw_info.min_val) * draw_info.block_height
+
+        color = draw_info.GRADIENTS[i%3]
+
+        pygame.draw.rect(draw_info.window,color,(x,y,draw_info.block_width,draw_info.height))
 
 def main():
     run = True
     clock = pygame.time.Clock()
 
     info = Information(800,600,GenerateList(50,0,100))
-
     while run:
         clock.tick(60)
 
-        pygame.display.update()
+        draw(info)
 
         for event in pygame.event.get():
             if event == pygame.QUIT:
